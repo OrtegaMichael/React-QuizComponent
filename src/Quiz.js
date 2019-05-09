1e53a6158f34b30f42c069b07ca412a663b197f5
Michael Ortega <itimetoshinei@hotmail.com>
2019-05-09 14:37:17 -0400

    misspelling

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,7 +8,7 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
-    renter() {
+    render() {
         return (
         <div>
             <div className="QuizQuestion">
